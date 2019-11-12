10daf18e658b69d400ecd2cc37b888634ac81210
Kristian Payne <kristianpayne@edu732A.kent.ac.uk>
2019-11-12 14:39:12 +0000

    Database connection in testAPI.js successful

--- api/routes/testAPI.js
+++ api/routes/testAPI.js
@@ -1,5 +1,23 @@
 var express = require('express');
+var mysql = require('mysql');
+
 var router = express.Router();
+
+const connection = mysql.createConnection({
+    host: 'dragon',
+    user: 'c37_cablink',
+    password: 'wlindl!',
+    database: 'c37_cablink'
+});
+
+connection.connect(err => {
+    if(err)
+    {
+        return err;
+    }else{
+        console.log('Database connection successful');
+    }
+});
 
 router.get('/', function(req, res, next) {
     res.send('API is working properly');
