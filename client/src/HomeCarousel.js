2c37e283a35da38d5b1a42bd4c0cd4a0dcdda163
S.D.Eyre <sde20@kent.ac.uk>
2020-01-23 12:29:38 +0000

    Add images to home carousel

--- client/src/HomeCarousel.js
+++ client/src/HomeCarousel.js
@@ -8,39 +8,39 @@ class HomeCarousel extends Component {
                 <Carousel.Item>
                     <img
                         className="d-block w-100"
-                        src={require('./images/solid-bg.png')}
-                        alt="First slide"
+                        src={require('./images/cab-interior.png')}
+                        alt="Cab Interior"
                         height='400px'
                     />
                     <Carousel.Caption>
-                        <h3>First slide label</h3>
-                        <p>Something about how easy it is to book cabs with cablink.</p>
+                        <h3>Book cabs with ease</h3>
+                        <p>Book your journey on the move with our app.</p>
                     </Carousel.Caption>
                 </Carousel.Item>
                 <Carousel.Item>
                     <img
                         className="d-block w-100"
-                        src={require('./images/solid-bg.png')}
-                        alt="Third slide"
+                        src={require('./images/cab-exterior.png')}
+                        alt="Cab Exterior"
                         height='400px'
                     />
 
                     <Carousel.Caption>
-                        <h3>Second slide label</h3>
-                        <p>Something about saving money with cablink.</p>
+                        <h3>Competitive Prices</h3>
+                        <p>Our service provides as many different quotes as possible, giving you the best value for money!</p>
                     </Carousel.Caption>
                 </Carousel.Item>
                 <Carousel.Item>
                     <img
                         className="d-block w-100"
-                        src={require('./images/solid-bg.png')}
+                        src={require('./images/street-overhead.png')}
                         alt="Third slide"
                         height='400px'
                     />
 
                     <Carousel.Caption>
-                        <h3>Third slide label</h3>
-                        <p>Something else thats cool.</p>
+                        <h3>Journey Flexibility</h3>
+                        <p>Our service allows you to stop off along the way during your journey!</p>
                     </Carousel.Caption>
                 </Carousel.Item>
             </Carousel>
