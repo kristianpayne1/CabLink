4e67618d272e4c6bad80a1fd5a71f43488329c48
Kristian Payne <kjp29@kent.ac.uk>
2020-01-23 19:22:50 +0000

    Added pick up and drop off inputs, centered sidebar toggle, and better sidebar styling

--- client/src/SideBar.js
+++ client/src/SideBar.js
@@ -2,6 +2,9 @@ import React, { Component } from 'react';
 import Button from 'react-bootstrap/Button';
 import Accordion from 'react-bootstrap/Accordion';
 import Card from 'react-bootstrap/Card';
+import InputGroup from 'react-bootstrap/InputGroup';
+import FormControl from 'react-bootstrap/FormControl';
+import Form from 'react-bootstrap/Form';
 
 class SideBar extends Component {
     render() {
@@ -14,7 +17,34 @@ class SideBar extends Component {
                             Choose pick-up & drop off
                          </Accordion.Toggle>
                         <Accordion.Collapse eventKey="0">
-                            <Card.Body>This is where user will choose pickup/drop off locations and time</Card.Body>
+                            <Card.Body>
+                                <Form>
+                                    <Form.Group controlId="formPickUp">
+                                        <InputGroup className="mb-3">
+                                            <FormControl
+                                                placeholder="Pick up location"
+                                                aria-label="Pick up location"
+                                                aria-describedby="basic-addon2"
+                                            />
+                                            <InputGroup.Append>
+                                                <Button variant="outline-primary">Search</Button>
+                                            </InputGroup.Append>
+                                        </InputGroup>
+                                    </Form.Group>
+                                    <Form.Group controlId="formDropOff">
+                                        <InputGroup className="mb-3">
+                                            <FormControl
+                                                placeholder="Drop off location"
+                                                aria-label="Drop off location"
+                                                aria-describedby="basic-addon2"
+                                            />
+                                            <InputGroup.Append>
+                                                <Button variant="outline-primary">Search</Button>
+                                            </InputGroup.Append>
+                                        </InputGroup>
+                                    </Form.Group>
+                                </Form>
+                            </Card.Body>
                         </Accordion.Collapse>
                     </Card>
                     <Card>
