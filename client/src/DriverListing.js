0b418be4f01d9a55a88bb40061d8cf758891a72b
Kristian Payne <kjp29@kent.ac.uk>
2020-02-05 17:12:31 +0000

    Removed searchInput has it caused issues and added sortby to driver listing

--- client/src/DriverListing.js
+++ client/src/DriverListing.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import DriverCard from './DriverCard.js';
+import Dropdown from 'react-bootstrap/Dropdown';
+import DropdownButton from 'react-bootstrap/DropdownButton';
 
 class DriverListing extends Component {
 
@@ -22,6 +24,17 @@ class DriverListing extends Component {
     render() {
         return (
             <div>
+                <DropdownButton
+                    key='dropdown'
+                    id={'sortby-dropdown'}
+                    size="sm"
+                    variant="secondary-light"
+                    title="Sort by"
+                >
+                    <Dropdown.Item eventKey="1">Recommended</Dropdown.Item>
+                    <Dropdown.Item eventKey="2">Fastest response</Dropdown.Item>
+                    <Dropdown.Item eventKey="3">Lowest price</Dropdown.Item>
+                </DropdownButton>
                 {this.listDrivers()}
             </div>
         );
--- client/src/SearchLocationForm.js
+++ client/src/SearchLocationForm.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
-import SearchInput from './SearchInput.js';
+import InputGroup from 'react-bootstrap/InputGroup';
+import FormControl from 'react-bootstrap/FormControl';
+//import SearchInput from './SearchInput.js';
 
 class SearchLocationForm extends Component {
 
     handleCurrentLocation = () => {
-        let pickupSearch = document.getElementById('autocomplete');
+        let pickupSearch = document.getElementById('pickupLocation');
         pickupSearch.value = this.props.currentLat + ' ' + this.props.currentLong;
     }
 
@@ -14,13 +16,32 @@ class SearchLocationForm extends Component {
         return (
             <Form>
                 <Form.Group>
-                    <SearchInput name='Pick up location' id='autocomplete'/>
+                    <InputGroup className="mb-3">
+                        <FormControl id='pickupLocation'
+                            placeholder='Pick up location'
+                            aria-label='Puck up location'
+                            aria-describedby="basic-addon2"
+                            value=''
+                        />
+                        <InputGroup.Append>
+                            <Button variant="outline-primary">Search</Button>
+                        </InputGroup.Append>
+                    </InputGroup>
                     <Button variant="outline-primary" size="sm" onClick={this.handleCurrentLocation} block>
                         Use current location
                      </Button>
                 </Form.Group>
                 <Form.Group>
-                    <SearchInput name='Drop off location' id='dropOff'/>
+                <InputGroup className="mb-3">
+                        <FormControl id='dropoffLocation'
+                            placeholder='Drop off location'
+                            aria-label='Drop off location'
+                            aria-describedby="basic-addon2"
+                        />
+                        <InputGroup.Append>
+                            <Button variant="outline-primary">Search</Button>
+                        </InputGroup.Append>
+                    </InputGroup>
                 </Form.Group>
             </Form>
         );
