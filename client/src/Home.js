4ed475cbcf4be103e1d24c5f56a0c96707e73e8d
Kristian Payne <kjp29@kent.ac.uk>
2020-02-21 16:10:46 +0000

    Slight changes to home page description

--- client/src/Home.js
+++ client/src/Home.js
@@ -10,7 +10,7 @@ class Home extends Component {
                 <HomeCarousel/>
                 <div>
                     <h1 id="intro">What We Do</h1>
-                    <p id="desc">CabLink provides an innovative service, allowing people to find a collection of cab companies in one place. This allows users to save money, time and the stress of arranging transportation throughout the Canterbury area.</p>
+                    <p id="desc">CabLink provides an innovative service, allowing people to easily find and compare cab companies in one place. This allows users to save money, time and the stress of arranging transportation throughout the Canterbury area.</p>
                     <Button id ="bookbutton" variant="outline-primary" href="./booking">Book Cab</Button>
                 </div>
             </div>
